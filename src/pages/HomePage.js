60f3f575e3b342b98784eba3aa317a14960a8ebe
WMCharles <charles.wafula@student.moringaschool.com>
2023-04-14 10:25:34 +0300

    create pages

---
+++ src/pages/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div>
+        <h2>You are logged in to the home page</h2>
+    </div>
+  )
+}
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function LoginPage() {
+  return (
+    <div>
+        <form action="">
+            <input type="text" name='username' placeholder='Enter username' />
+            <input type="password" name="password" id=""  placeholder='Enter password'/>
+            <input type="submit"/>
+        </form>
+    </div>
+  )
+}
