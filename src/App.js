44f44f2c76de4705e624d0dfb157621f182fb0aa
WMCharles <charles.wafula@student.moringaschool.com>
2023-04-14 10:29:02 +0300

    create navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route path='/' element={<HomePage />} exact />
           <Route path='/login' element={<LoginPage />} />
