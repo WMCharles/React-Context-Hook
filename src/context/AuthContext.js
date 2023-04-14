50cf288ce1e9931673caa349f3a4a10c2121d207
WMCharles <charles.wafula@student.moringaschool.com>
2023-04-14 16:26:34 +0300

    fetch data

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { createContext, useState, useEffect } from 'react'
 import jwt_decode from 'jwt-decode'
 import { useNavigate } from 'react-router-dom'
@@ -88,7 +89,7 @@ export const AuthProvider = ({ children }) => {
     let contextData = {
         // variables
         user: user,
-
+        authTokens: authTokens,
         // functions
         loginUser: loginUser,
         logoutUser: logoutUser
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,43 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useContext, useEffect, useState } from 'react'
+import AuthContext from '../context/AuthContext'
 
 export default function HomePage() {
+  let [notes, setNotes] = useState([])
+  let { authTokens, logoutUser } = useContext(AuthContext)
+
+  useEffect(() => {
+    getNotes()
+  }, [])
+
+
+  let getNotes = async () => {
+    let response = await fetch('http://localhost:8000/api/notes/', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + String(authTokens.access)
+      }
+    })
+    let data = await response.json()
+
+    if (response.status === 200) {
+      setNotes(data)
+    } else if (response.statusText === 'Unauthorized') {
+      logoutUser()
+    }
+
+  }
   return (
     <div>
-        <h2>You are logged in to the home page</h2>
+      <p>You are logged to the home page!</p>
+
+
+      <ul>
+        {notes.map(note => (
+          <li key={note.id} >{note.title}</li>
+        ))}
+      </ul>
     </div>
   )
 }
